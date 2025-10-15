b2eb2dff6b20658f2e9b24757f140c13a18daac3
Suhail7985 <suhailmansoori7985@gmail.com>
2025-10-15 23:36:12 +0530

    update

--- 14. Longest Common Prefix/solution.cpp
+++ 14. Longest Common Prefix/solution.cpp
@@ -8,9 +8,18 @@ public:
         string first = strs[0];
         string last = strs.back();
 
-…            }
+        string result = "";
+        int i = 0;
+        while(i<first.size() && i<last.size()){
+            if(first[i] == last[i]){
+                result += first[i];
+                i++;
+            }
+            else{
+                break;
+            }
         }
-        return result;
+               return result;
         
     }
 };
