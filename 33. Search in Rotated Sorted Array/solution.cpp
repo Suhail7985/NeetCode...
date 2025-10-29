c435822277a90ba8f71a5a444b9fa27b6213cfe4
Suhail7985 <suhailmansoori7985@gmail.com>
2025-10-29 11:16:06 +0530

    update

--- 33. Search in Rotated Sorted Array/solution.cpp
+++ 33. Search in Rotated Sorted Array/solution.cpp
@@ -1,3 +1,4 @@
+//Brute Force Approach
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -10,5 +11,34 @@ public:
         }
       }
        return first; 
+    }
+};
+
+//Optimal Approach
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int low = 0 , high = nums.size() - 1;
+      while(low<=high){
+        int mid = (low+high)/2;
+        if(nums[mid]==target) return mid;
+        if(nums[low]<=nums[mid]){
+          if(nums[low]<=target && target <= nums[mid]){
+            high = mid-1;
+          }
+          else{
+            low= mid + 1;
+          }
+        }
+        else{
+          if(nums[mid]<=target && target <= nums[high]){
+            low = mid + 1;
+          }
+          else{
+            high = mid - 1;
+          }
+        }
+      }
+      return -1; 
     }
 };
